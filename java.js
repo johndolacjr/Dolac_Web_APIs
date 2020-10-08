4a890bc070bf259f25dca48528fd241ec589a2d3
johndolac <johndolacjr@gmail.com>
2020-10-08 11:08:11 -0600

    initial commit

---
+++ java.js
@@ -0,0 +1,273 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+function startGame() {
+    console.log('Started')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = ('Restart')
+        startButton.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+const questions = [
+    {
+        question: 'How doeas a FOR loop start?',
+        answers: [
+            {text: 'for (i<=5; i++)', correct: false},
+            {text: 'for (i = 0; <=5; i++)', correct: true},
+            {text: 'for i = 1 to 5', correct: false},
+            {text: 'for (i = 0; i <=5)', correct: false}
+        ]
+    },
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        answers: [
+            {text: '<script>', correct: true},
+            {text: '<javascript>', correct: false},
+            {text: '<scripting>', correct: false},
+            {text: '<scripts>', correct: false}
+        ]
+    },
+    {
+        question: 'How do you write "Hello World" in an alert box?',
+        answers: [
+            {text: 'alertBox("Hello World");', correct: false},
+            {text: 'msgBox("Hello World");', correct: false},
+            {text: 'msg("Hello World");', correct: false},
+            {text: 'alert("Hello World");', correct: true}
+        ]
+    },
+    {
+        question: 'How do you create a function in JavaScript?',
+        answers: [
+            {text: 'function myFunction()', correct: true},
+            {text: 'function:myFunction()', correct: false},
+            {text: 'function = myFunction()', correct: false},
+            {text: 'function myFunction[]', correct: false}
+        ]
+    },
+    {
+        question: 'How do you write an IF statement in JavaScript?',
+        answers: [
+            {text: 'if i == 5 then', correct: false},
+            {text: 'if i = 5 then', correct: false},
+            {text: 'if(i == 5)', correct: true},
+            {text: 'if i = 5', correct: false}
+        ]
+    },
+    {
+        question: 'What copies array elements within the array, to and from specified positions?',
+        answers: [
+            {text: 'copyMove()', correct: false},
+            {text: 'copyWithin()', correct: true},
+            {text: 'copyArrayElements()', correct: false},
+            {text: 'copy()', correct: false}
+        ]
+    },    {
+        question: 'What does forEach() do?',
+        answers: [
+            {text: 'It does nothing', correct: false},
+            {text: 'creates an array element for each function', correct: false},
+            {text: 'calls a function for each array element', correct: true},
+            {text: 'Creates a for loop', correct: false}
+        ]
+    },
+
+]
+
+//  function storedScores ()
+
+
+// When the quiz starts, a countdown timer starts and is displayed
+
+
+// Incorrect answers cause time to be subtracted from the timer
+
+
+// The game ends when all questions have been answered or the timer gets to zero
+
+
+// After the game, users can store their high scores in local storage, and can view those high scores by clicking a link
+
+
+// The application only has to resemble the example (a quiz with questions), not be an exact match
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var questions = [
+//     {
+//         prompt: 'How doeas a FOR loop start?\n(a) for (i<=5; i++)\n\ (b) for (i = 0; <=5; i++)\n(c) for i = 1 to 5\n(d) for (i = 0; i <=5)',
+//         answer: 'b'
+//     },
+//     {
+//         prompt: 'How doeas a FOR loop start?\n(a) for (i<=5; i++)\n\ (b) for (i = 0; <=5; i++)\n(c) for i = 1 to 5\n(d) for (i = 0; i <=5)',
+//         answer: 'b'
+//     },
+//     {
+//         prompt: 'How doeas a FOR loop start?\n(a) for (i<=5; i++)\n\ (b) for (i = 0; <=5; i++)\n(c) for i = 1 to 5\n(d) for (i = 0; i <=5)',
+//         answer: 'b'
+//     },
+//     {
+//         prompt: 'How doeas a FOR loop start?\n(a) for (i<=5; i++)\n\ (b) for (i = 0; <=5; i++)\n(c) for i = 1 to 5\n(d) for (i = 0; i <=5)',
+//         answer: 'b'
+//     },
+//     {
+//         prompt: 'How doeas a FOR loop start?\n(a) for (i<=5; i++)\n\ (b) for (i = 0; <=5; i++)\n(c) for i = 1 to 5\n(d) for (i = 0; i <=5)',
+//         answer: 'b'
+//     },
+//     {
+//         prompt: 'How doeas a FOR loop start?\n(a) for (i<=5; i++)\n\ (b) for (i = 0; <=5; i++)\n(c) for i = 1 to 5\n(d) for (i = 0; i <=5)',
+//         answer: 'b'
+//     },
+// ];
+
+// var score = 0;
+
+// for (var i=0; i < questions.length; i++){
+//     var response = window.prompt(questions[i].prompt); 
+//     if (response === questions[i]).answer){
+//         score++;
+//         alert("Correctamondo!!");
+//     } 
+    
+//     else {
+//         alert("Womp Womp");
+//     }
+// }
+// alert ("You got " + score + "/" + questions.length);
+
+
+        
+        
+        
+// //= document.getElementById(mainContent); 
+
+// // var questionsArray = [
+// //     {
+// //         q1: 'How doeas a FOR loop start?',
+// //         answers: ['for (i<=5; i++)', 'for (i = 0; <=5; i++)', 'for i = 1 to 5', 'for (i = 0; i <=5)'],
+// //         correctAnswer: 'for (i = 0; <=5; i++)',
+// //     },
+    
+// //     {
+// //         q2: 'How doeas a FOR loop start?',
+// //         answers: ['for (i<=5; i++)', 'for (i = 0; <=5; i++)', 'for i = 1 to 5', 'for (i = 0; i <=5)'],
+// //         correctAnswer: 'for (i = 0; <=5; i++)',
+// //     },
+    
+// //     {
+// //         q3: 'How doeas a FOR loop start?',
+// //         answers: ['for (i<=5; i++)', 'for (i = 0; <=5; i++)', 'for i = 1 to 5', 'for (i = 0; i <=5)'],
+// //         correctAnswer: 'for (i = 0; <=5; i++)',
+// //     },
+    
+// //     {
+// //         q4: 'How doeas a FOR loop start?',
+// //         answers: ['for (i<=5; i++)', 'for (i = 0; <=5; i++)', 'for i = 1 to 5', 'for (i = 0; i <=5)'],
+// //         correctAnswer: 'for (i = 0; <=5; i++)',
+// //     },
+// // ]; 
+
+
+// // I need to display the questions w/ random answers
+// var questionHtml = "questionArray",
+// content.innerHTML = questionHtml,
+
+
+// //need a function to check the user selection w/ 
+
+// // display a message if correct. If incorrect decremeant time and state its wrong
+
+// //game ends when timer hits Zero or no more questions are left 
